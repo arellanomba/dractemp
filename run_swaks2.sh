131bf4225f95882eac870b251d00c368459a1a03
arellanomba <46618948+arellanomba@users.noreply.github.com>
2024-05-20 13:04:37 -0700

    Add files via upload

---
+++ run_swaks2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the Conda environment
+source /home/warellano/miniconda3/bin/activate base
+
+# Optionally, activate a specific Conda environment
+# source /home/warellano/miniconda3/bin/activate your_env_name
+
+# Run the Python script
+/home/warellano/code/dractemp/swaks2.sh
